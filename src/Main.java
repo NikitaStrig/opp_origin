2f7759da07e45af33faddb1166333449ea6ea74f
nikitastrigunkov <demonkurt1995@gmail.com>
2023-01-12 13:13:40 +0300

    OOP Car new param

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Car[] mass = new Car[6];
-        mass[0] = new Car("Lada","Granta",1.7, "Yalow", 2015,"Russia");
-        mass[1] = new Car("BMW","Z8",3.0, "Black", 2021,"Germany");
-        mass[2] = new Car("Audi","A8 50 L TDI quattr",1.7, "Black", 2020,"Germany");
-        mass[3] = new Car("Kia","Sportage 4-го поколения",2.4, "Red", 2018,"South Korea");
-        mass[4] = new Car("Hyundai","Avante",1.6, "Orange", 2016,"South Korea");
-        mass[5] = new Car("","",0, null, 0,null);
+        transport.Car[] mass = new transport.Car[6];
+        mass[0] = new transport.Car("Lada","Granta",1.7, "Yalow", 2015,"Russia", "Meh", "Sedan", "C337PV777", 5,10);
+        mass[1] = new transport.Car("BMW","Z8",3.0, "Black", 2021,"Germany", "Meh", "Sedan", "C337PV777", 5, 4);
+        mass[2] = new transport.Car("Audi","A8 50 L TDI quattr",1.7, "Black", 2020,"Germany", "Meh", "Sedan", "C337PV777", 5, 11);
+        mass[3] = new transport.Car("Kia","Sportage 4-го поколения",2.4, "Red", 2018,"South Korea", "Meh", "Sedan", "C337PV777", 5, 1);
+        mass[4] = new transport.Car("Hyundai","Avante",1.6, "Orange", 2016,"South Korea", "Meh", "Sedan", "C337PV777", 5, 5);
+        mass[5] = new transport.Car("","",0, null, 0,null, "Meh", "Sedan", "C337PV777", 5, 3);
 
         for (int i = 0; i < mass.length; i++) {
             System.out.println(mass[i]);
---
+++ src/transport/Car.java
@@ -0,0 +1,125 @@
+package transport;
+public class Car {
+   final String brand;
+   final String  model;
+   private double engineVolume;
+   private String  color;
+   final   int year;
+   final String country;
+   private String transmission;
+   final String bodyType;
+   private String registrationNumber;
+   final int numberOfSeats;
+   private int mSignTires;
+   private boolean signTires;
+
+
+   public String getBrand() {
+      return brand;
+   }
+
+   public String getModel() {
+      return model;
+   }
+
+
+   public double getEngineVolume() {
+      return engineVolume;
+   }
+
+   public void setEngineVolume(double engineVolume) {
+      this.engineVolume = engineVolume;
+   }
+
+   public String getColor() {
+      return color;
+   }
+
+   public void setColor(String color) {
+      this.color = color;
+   }
+
+   public int getYear() {
+      return year;
+   }
+
+   public String getCountry() {
+      return country;
+   }
+   public String getTransmission() {
+      return transmission;
+   }
+
+   public void setTransmission(String transmission) {
+      this.transmission = transmission;
+   }
+
+   public String getBodyType() {
+      return bodyType;
+   }
+
+   public String getRegistrationNumber() {
+      return registrationNumber;
+   }
+
+   public void setRegistrationNumber(String registrationNumber) {
+      this.registrationNumber = registrationNumber;
+   }
+
+   public int getNumberOfSeats() {
+      return numberOfSeats;
+   }
+
+   public boolean getSignTires() {
+      return signTires;
+   }
+
+   public Car(String brand, String model, double engineVolume, String color, int year, String country,
+              String transmission, String bodyType, String registrationNumber, int numberOfSeats, int mSignTires) {
+      this.brand = brand;
+      this.model = model;
+      this.engineVolume = engineVolume;
+      this.color = color;
+      this.year = year;
+      this.country = country;
+      this.transmission = transmission;
+      this.bodyType = bodyType;
+      this.registrationNumber = registrationNumber;
+      this.numberOfSeats = numberOfSeats ;
+      this.mSignTires = mSignTires;
+
+      if (this.engineVolume <= 0) {
+         this.engineVolume = 1.5;
+      }
+      if (this.color == "" || this.color == null) {
+         this.color = "White";
+      }
+      if (this.transmission == "" || this.transmission == null){
+         this.transmission = "Auto";
+      }
+      if (this.registrationNumber == "" || this.registrationNumber == null){
+         this.transmission = "A000AA000";
+      }
+      if (this.mSignTires >= 3 && this.mSignTires <= 10){
+         this.signTires = false;
+      }
+      else {this.signTires = true;}
+   }
+
+   @Override
+   public String toString() {
+      return "Car{" +
+              "brand='" + brand + '\'' +
+              ", model='" + model + '\'' +
+              ", engineVolume=" + engineVolume +
+              ", color='" + color + '\'' +
+              ", year=" + year +
+              ", country='" + country + '\'' +
+              ", transmission='" + transmission + '\'' +
+              ", bodyType='" + bodyType + '\'' +
+              ", registrationNumber='" + registrationNumber + '\'' +
+              ", numberOfSeats=" + numberOfSeats +
+              ", signTires=" + signTires +
+              '}';
+   }
+}
